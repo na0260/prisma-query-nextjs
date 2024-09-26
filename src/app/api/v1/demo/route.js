174e48bdf41b9d33e4d81032bd45ad8477bb836c
na0260 <samjr.008@gmail.com>
2024-09-27 03:23:24 +0600

    relational find

--- src/app/api/v1/demo/route.js
+++ src/app/api/v1/demo/route.js
@@ -61,4 +61,19 @@ export async function POST(req, res) {
     }catch (e) {
         return NextResponse.json({status: "Failed", data: e});
     }*/
+    // Relational find
+    /*try{
+        const prisma = new PrismaClient();
+        const result = await prisma.User.findMany({
+            include:{
+                profile:true,
+                post:true
+            }
+        });
+
+        return NextResponse.json({status: "Success", data: result});
+
+    }catch (e) {
+        return NextResponse.json({status: "Failed", data: e});
+    }*/
 }
